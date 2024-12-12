861d2a2724cc48032af52903c0e99389de3baad8
Sveta Trusova <Svetatrusova2.0@yandex.ru>
2024-12-12 19:42:10 +0300

    renamed files and chenged structure

---
+++ lab1/task5/src/task5.py
@@ -0,0 +1,27 @@
+from lab1.utils import selectionsort, read_input, write_output, normVid, memory_usage_process
+path_input = "../txtf/input.txt"
+path_output = "../txtf/output.txt"
+
+
+def to_sort(data):
+    sorted_data = selectionsort(data)
+    norm_sorted_data = normVid(sorted_data)
+    return norm_sorted_data
+
+
+def main(path_input, path_output):
+    n, data = read_input(path_input)
+    print("Входные данные:")
+    print(n)
+    print(*data)
+    norm_sorted_data = to_sort(data)
+    print("Результат:")
+    print(norm_sorted_data)
+    write_output(norm_sorted_data, path_output)
+
+
+print(f"Memory used by the current process: {memory_usage_process()} Megabytes")
+
+
+if __name__ == '__main__':
+    main(path_input, path_output)
